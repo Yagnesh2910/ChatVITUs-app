f3e3a7e5d68c991549fb4160543b12b25704bb1e
Yagnesh <143516503+Yagnesh2910@users.noreply.github.com>
2025-02-16 23:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,4 +207,4 @@ def ask():
     return jsonify({'response': response})
 
 if(__name__== '__main__'):
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
