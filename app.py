faaaaeeccfbfb62fd6a6fba2afd4aa6aee25647a
Yagnesh <143516503+Yagnesh2910@users.noreply.github.com>
2025-02-17 22:38:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 from Levenshtein import distance as levenshtein_distance
 from fuzzywuzzy import process
+import os
 
 
 app = Flask(__name__)
@@ -207,5 +208,4 @@ def ask():
     return jsonify({'response': response})
 
 if(__name__== '__main__'):
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
